9c7436b04acecccb24878392d1837011f90d46e8
Carlos.VazPI <carlos@intellectionsoftware.com>
2019-11-15 12:25:19 +0000

    more actions

--- update_package.js
+++ update_package.js
@@ -1,9 +1,11 @@
 const fs = require('fs')
 
-let data = JSON.parse(fs.readFileSync('package.json'))
-let newData = {
+const branchName = process.argv[2]
+const data = JSON.parse(fs.readFileSync('package.json'))
+const newData = {
   ...data,
-  version: data.version + 1
+  branchName,
+  version: data.version + 1,
 }
 fs.writeFileSync('package.json', JSON.stringify(newData))
 console.log(newData)
