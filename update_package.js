2e456d6adfd8aa132b3d61ec4381ff958e2327c6
Carlos.VazPI <carlos@intellectionsoftware.com>
2019-11-19 15:03:08 +0000

    More

--- update_package.js
+++ update_package.js
@@ -5,7 +5,6 @@ const data = JSON.parse(fs.readFileSync('package.json'))
 const newData = {
   ...data,
   branchName,
-  version: data.version + 1,
+  version: data.version + 1
 }
 fs.writeFileSync('package.json', JSON.stringify(newData))
-console.log(newData)
