4e6f6339ca3b43f4e8a155d6f1c02f96a55960f8
Carlos.VazPI <carlos@intellectionsoftware.com>
2019-11-19 13:52:04 +0000

    More

---
+++ do_stuff.js
@@ -0,0 +1,55 @@
+// const https = require('https')
+const { exec } = require('child_process')
+
+//
+// https.get('https://api.github.com/repos/CarlosVazPI/check_actions/git/ref/heads/master-more-5fb914b', (resp) => {
+//   let data = ''
+//   resp.on('data', (chunk) => {
+//     data += chunk;
+//   })
+// }).on('error', console.error)
+
+const branch = process.argv[2]
+exec('git clone https://github.com/Intellection/fluid.framework.git', (error, stdout, stderr) => {
+  if (error || stderr) {
+    console.log(error || stderr)
+  }
+  exec('cd fluid.framework', (error, stdout, stderr) => {
+    if (error || stderr) {
+      console.log(error || stderr)
+    }
+    exec(`node update_package ${branch}`, (error, stdout, stderr) => {
+      if (error || stderr) {
+        console.log(error || stderr)
+      } else {
+        console.log(stdout)
+      }
+    })
+  })
+})
+
+
+// const data = {
+//   base: 'master',
+//   branch: `${branch}-extra`,
+//   title: `Point to ui branch ${branch}`,
+//
+// }
+// const options = {
+//   hostname: 'https://api.github.com',
+//   port: 443,
+//   path: '/repos/CarlosVazPI/check_actions/pulls',
+//   method: 'POST',
+//   headers: {
+//     'Content-Type': 'application/json',
+//     'Content-Length': data.length
+//   }
+// }
+//
+// const req = https.request(options)
+//
+//
+//
+//
+// req.write(data)
+// req.end()
