7d79468fbe306b4e6b8e0a6200b78b6b6145f372
Carlos.VazPI <carlos@intellectionsoftware.com>
2019-11-19 14:55:09 +0000

    More

--- do_stuff.js
+++ do_stuff.js
@@ -1,6 +1,24 @@
 // const https = require('https')
 const { exec } = require('child_process')
 
+const run = (command, options) => () => {
+  return new Promise((resolve, reject) => {
+    console.log("\x1b[36m", '> ', command)
+    exec(command, options, (error, stdout, stderr) => {
+      if (error) {
+        console.log("\x1b[31m", '< ', command, error)
+        exec(`echo "Error: ${error, stderr}" >> .log`)
+      } else {
+        console.log("\x1b[32m", '< ', command, stdout)
+        if (stderr) {
+          console.log("\x1b[37m", '  ', stderr)
+        }
+        resolve(stdout)
+      }
+    })
+  })
+}
+
 //
 // https.get('https://api.github.com/repos/CarlosVazPI/check_actions/git/ref/heads/master-more-5fb914b', (resp) => {
 //   let data = ''
@@ -10,69 +28,20 @@ const { exec } = require('child_process')
 // }).on('error', console.error)
 
 const branch = process.argv[2]
-exec('rm .log')
-exec('touch .log')
-exec('git clone https://github.com/Intellection/fluid.framework.git', (error, stdout, stderr) => {
-  if (error || stderr) {
-    console.log('Error', error || stderr)
-    exec(`echo "Error: ${error || stderr}" >> .log`)
-    return
-  }
-  exec('cd fluid.framework', (error, stdout, stderr) => {
-    if (error || stderr) {
-      console.log('Error', error || stderr)
-      exec(`echo "Error: ${error || stderr}" >> .log`)
-      return
-    }
-    exec(`git checkout -b automatic_${branch}`, (error, stdout, stderr) => {
-      if (error || stderr) {
-        console.log('Error', error || stderr)
-        exec(`echo "Error: ${error || stderr}" >> .log`)
-        return
-      }
-      exec(`node update_package ${branch}`, (error, stdout, stderr) => {
-        if (error || stderr) {
-          console.log('Error', error || stderr)
-          exec(`echo "Error: ${error || stderr}" >> .log`)
-          return
-        }
-        exec(`git commit -am "Automatically pointing to "${branch}"`, (error, stdout, stderr) => {
-          if (error || stderr) {
-            console.log('Error', error || stderr)
-            exec(`echo "Error: ${error || stderr}" >> .log`)
-            return
-          }
-          exec('git push', (error, stdout, stderr) => {
-            if (error || stderr) {
-              console.log('Error', error || stderr)
-              exec(`echo "Error: ${error || stderr}" >> .log`)
-              return
-            } else {
-              exec(`echo "Success: ${stdout}" >> .log`)
-              console.log(stdout)
-            }
-          })
-        })
-      })
-    })
-  })
-})
-
-
-// const data = {
-//   base: 'master',
-//   branch: `${branch}-extra`,
-//   title: `Point to ui branch ${branch}`,
-//
-// }
-// const options = {
-//   hostname: 'https://api.github.com',
-//   port: 443,
-//   path: '/repos/CarlosVazPI/check_actions/pulls',
-//   method: 'POST',
-//   headers: {
-//     'Content-Type': 'application/json',
-//     'Content-Length': data.length
+run('rm .log')()
+  .then(run('touch .log'))
+  .then(run('rm -rf fluid.framework'))
+  .then(run('git clone https://github.com/Intellection/fluid.framework.git'))
+  .then(run('cd fluid.framework'))
+  .then(run(`git checkout -b automatic_${branch}`, { cwd: './fluid.framework' }))
+  .then(run(`node ../update_package ${branch}`, { cwd: './fluid.framework' }))
+  .then(run(`git commit -am "Automatically pointing to '${branch}'"`, { cwd: './fluid.framework' }))
+  .then(run(`git push --set-upstream origin automatic_${branch}`, { cwd: './fluid.framework' }))
+// exec('git clone https://github.com/Intellection/fluid.framework.git', (error, stdout, stderr) => {
+//   if (error || stderr) {
+//     console.log('Error', error || stderr)
+//     exec(`echo "Error: ${error || stderr}" >> .log`)
+//     return
 //   }
 // }
 //
